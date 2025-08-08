b9cfa8380d7cfa9c7b50a276ada801f59470775a
Josiah Nyakwara <122626683+TJ-dot-1@users.noreply.github.com>
2025-08-08 17:22:05 +0300

    Add files via upload

---
+++ my list.py
@@ -0,0 +1,29 @@
+# 1. my_list
+my_list = []
+
+# 2. Append elements: 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+# my_list output [10, 20, 30, 40]
+
+# 3. Insert 15 at the second position (index 1)
+my_list.insert(1, 15)
+# my_list output [10, 15, 20, 30, 40]
+
+# 4. Extend with [50, 60, 70]
+my_list.extend([50, 60, 70])
+# my_list output [10, 15, 20, 30, 40, 50, 60, 70]
+
+# 5. Remove the last element
+my_list.pop(7)
+# my_list output [10, 15, 20, 30, 40, 50, 60]
+
+# 6. Sort in ascending order
+my_list.sort()
+# my_list output [10, 15, 20, 30, 40, 50, 60]
+
+# 7. Find and print the index of 30
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
